7e88445b85f116343e67f268b82c29d779257294
Massimiliano Brighindi brighissimo@gmail.com <brighissimo@gmail.com>
2026-05-07 20:08:48 +0200

    Update __init__.py

--- omnia_validation/__init__.py
+++ omnia_validation/__init__.py
@@ -10,17 +10,31 @@ Boundary:
 from .hashing import is_sha256_hex, sha256_bytes, sha256_file, sha256_text
 from .io import read_json, read_jsonl, write_json, write_jsonl
 from .metrics import compression_ratio, normalized_repetition_score, shannon_entropy
+from .schemas import (
+    ALLOWED_RESULT_STATUSES,
+    DEFAULT_BOUNDARY,
+    REQUIRED_RESULT_FIELDS,
+    is_valid_result_envelope,
+    require_valid_result_envelope,
+    validate_result_envelope,
+)
 
 __all__ = [
+    "ALLOWED_RESULT_STATUSES",
+    "DEFAULT_BOUNDARY",
+    "REQUIRED_RESULT_FIELDS",
     "compression_ratio",
     "is_sha256_hex",
+    "is_valid_result_envelope",
     "normalized_repetition_score",
     "read_json",
     "read_jsonl",
+    "require_valid_result_envelope",
     "sha256_bytes",
     "sha256_file",
     "sha256_text",
     "shannon_entropy",
+    "validate_result_envelope",
     "write_json",
     "write_jsonl",
 ]
